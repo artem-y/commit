ba48e506bbbd2bd47d49955e984959891c4a2258
Artem Yelizarov <52959979+artem-y@users.noreply.github.com>
2024-04-18 10:40:44 +0300

    Fix premature returning of empty config

--- internal/config/config.go
+++ internal/config/config.go
@@ -18,23 +18,22 @@ type commitConfig struct {
 func ReadCommitConfig() commitConfig {
 
 	configFilePath := helpers.DEFAULT_CONFIG_FILE_PATH
+	var cfg commitConfig
 
 	_, err := os.Stat(configFilePath)
-	if os.IsNotExist(err) {
-		return commitConfig{}
-	}
+	if err == nil {
 
-	file, err := os.ReadFile(configFilePath)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, helpers.Red("Error reading %s file: %v\n"), err, configFilePath)
-		os.Exit(1)
-	}
+		file, err := os.ReadFile(configFilePath)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, helpers.Red("Error reading %s file: %v\n"), err, configFilePath)
+			os.Exit(1)
+		}
 
-	var cfg commitConfig
-	err = json.Unmarshal(file, &cfg)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, helpers.Red("Error unmarshalling %s file: %v\n"), err, configFilePath)
-		os.Exit(1)
+		err = json.Unmarshal(file, &cfg)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, helpers.Red("Error unmarshalling %s file: %v\n"), err, configFilePath)
+			os.Exit(1)
+		}
 	}
 
 	if cfg.IssueRegex == "" {
