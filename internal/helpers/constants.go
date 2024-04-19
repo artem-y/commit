e8ea968666dafd914c341946e2688157aff6c372
Artem Yelizarov <52959979+artem-y@users.noreply.github.com>
2024-04-19 03:30:16 +0300

    #10: Set up default regex to default github branch style

--- internal/helpers/constants.go
+++ internal/helpers/constants.go
@@ -2,7 +2,7 @@ package helpers
 
 const (
 	DEFAULT_CONFIG_FILE_PATH    = ".commit.json"
-	DEFAULT_ISSUE_REGEX         = "#[0-9]+"
+	DEFAULT_ISSUE_REGEX         = "[0-9]+"
 	DEFAULT_OUTPUT_ISSUE_PREFIX = "[#"
 	DEFAULT_OUTPUT_ISSUE_SUFFIX = "]: "
 )
